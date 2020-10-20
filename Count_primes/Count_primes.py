e66ecd45a9b364207a0ed581a56f5584e109db0f
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-10-21 01:10:48 +0530

    Add files via upload

---
+++ Count_primes/Count_primes.py
@@ -0,0 +1,18 @@
+def count_primes2(num):
+    primes = [2]
+    x = 3
+    if num < 2:
+        return 0
+    while x <= num:
+        for y in primes:  # use the primes list!
+            if x%y == 0:
+                x += 2
+                break
+        else:
+            primes.append(x)
+            x += 2
+    print(primes)
+    return len(primes)
+
+num= int(input("Enter limit: "))
+count_primes2(num)
