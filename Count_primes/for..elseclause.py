1b0bdaac6c84a34a1d0a2f44e4efd98a3f43b8c7
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-10-21 01:12:35 +0530

    Add files via upload

---
+++ Count_primes/for..elseclause.py
@@ -0,0 +1,8 @@
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print( n, 'equals', x, '*', n/x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
