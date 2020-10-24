ccd13abfc0c44779ababe26dfdea13f785759192
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-10-24 20:28:54 +0530

    Update Finalprogram.py

--- TicTacToe/Finalprogram.py
+++ TicTacToe/Finalprogram.py
@@ -1,7 +1,7 @@
 from IPython.display import clear_output
 
 def display_board(board):
-    clear_output()  # Remember, this only works in jupyter!
+    clear_output()  # This only works in jupyter!
     
     print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
