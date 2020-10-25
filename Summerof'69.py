4f32a10cc39f75e8e1086f1eefd2ededb22ad019
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-10-25 19:15:15 +0530

    Update Summerof'69.py

--- Summerof'69.py
+++ Summerof'69.py
@@ -1,5 +1,6 @@
 """SUMMER OF '69: 
 Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 9 (every 6 will be followed by at least one 9). Return 0 for no numbers.
+(Also one of my fav songs from Bryan Adams!)
 
 summer_69([1, 3, 5]) --> 9
 summer_69([4, 5, 6, 7, 8, 9]) --> 9
