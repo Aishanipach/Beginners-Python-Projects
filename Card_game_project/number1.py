37a92f90cdd90ad141cccda6c036e73e00ba42bf
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-11-26 14:39:00 +0530

    Update number1.py

--- Card_game_project/number1.py
+++ Card_game_project/number1.py
@@ -1,3 +1,10 @@
+cards=["Ace", "King", "Queen", "Jack", "One", "Two", "Three", "Four", "Five",
+             "Six", "Seven", "Eight", "Nine", "Ten"]
+suits=["Heart", "Spade", "Diamond","Club"]
+
+points={"Ace":14, "King":13, "Queen":12, "Jack":11, "One":1, "Two":2, "Three":3, "Four":4, "Five":5,
+             "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10}
+
 class Cards:
 
     def __init__(self, suit, rank):
@@ -6,3 +13,10 @@ class Cards:
     
     def __str__(self):
         return self.rank 
+
+def main():
+     print("\t WELCOME TO the CARD GAME- WAR!\n\n")
+     print("Let's shufle,divide and start!")
+     print(points)
+
+main()
