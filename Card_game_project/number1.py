daa925228e35170d3a4f86b8d2a52961d5a3b8d2
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-12-01 02:57:01 +0530

    Update number1.py

--- Card_game_project/number1.py
+++ Card_game_project/number1.py
@@ -19,30 +19,135 @@ class Cards:
 class Create_Deck:
     def __init__(self):
         self.deck=[]
-        
 
         for suit in suits:
             for card in cards:
                 new_card=Cards(suit, card)
                 self.deck.append(new_card)
+
         random.shuffle(self.deck) #shuffled the created deck
 
     def deal_1(self):
         return self.deck.pop()
     
-    def add_cards(self,added_cards, name):
+   
+    def split_deck(self):   
+        d1=[]
+        d2=[]
+        for x in range(26):
+            d1.append(self.deal_1())
+            d2.append(self.deal_1())
+        
+        return d1, d2
+
+class Gameplay:
+
+    def __init__(self, name,deck):
+        self.name=name
+        self.deck=deck
+    
+    def draw(self):
+        return self.deck.pop()
+        
+
+    def add_cards(self,added_cards):
         if type(added_cards) == type([]):
             self.deck.extend(added_cards)
         else:
             self.deck.append(added_cards)
-        print(f"{name} player has {len(self.deck)} cards")
-
+        print(f"{self.name} player has {len(self.deck)} cards")
+    
+def check_value(card):
+    return( points[card])
 
 def main():
     print("\t WELCOME TO the CARD GAME- WAR!\n\n")
-    print("Let's shufle,divide and start!")
+    print("Let's shufle,divide and start!") 
     d=Create_Deck()
-    d.deal_1()
-    d.add_cards(["2", "Ace","hey"], "P1")
+    play1, play2= d.split_deck()
+    p1=Gameplay("Player 1", play1)
+    p2=Gameplay("Player 2", play2)
+    
+
+    outcome=False
+
+    while(outcome==False):
+
+        if(len(p1.deck)==0):
+            print("Player 1 is out of cards")
+            print("Player 2 wins!" )
+            outcome=True
+            break
+        elif(len(p2.deck)==0):
+            print("Player 2 is out of cards")
+            print("Player 1 wins!" )
+            outcome=True
+            break
+        card1=str(p1.draw())
+        card2=str(p2.draw())
+
+        #Check which card has bigger value
+        if(check_value(card1)>check_value(card2)):
+            print("Player one won this round")
+            p1.add_cards(card2)
+        elif(check_value(card1)<check_value(card2)):
+            print("Player two won this round")
+            p2.add_cards(card1)
+
+    #If same value, WAR, draw five cards each
+        elif(check_value(card1)==check_value(card2)):
+            print("\tWAR!")
+
+            if(len(p1.deck)<5):
+                print("Player 1 is out of cards, NO WAR.")
+                print("Player 2 wins!" )
+                outcome=True
+                break
+            elif(len(p2.deck)<5):
+                print("Player 2 is out of cards")
+                print("Player 1 wins!" )
+                outcome=True
+                break
+            
+            else:
+                round=1
+                while(round>0):
+                    if(check_value(str(p1.deck[-1]))>check_value(str(p2.deck[-1]))):
+                        #Add cards onto winners pile
+                        ctr=0
+                        list_won=[]
+                        #Pop cards from loser's deck
+                        while(ctr<5):
+                            ctr=ctr+1
+                            i=p2.draw()
+                            list_won.append(i)
+                            
+                          
+                        p1.add_cards(list_won)
+                        print("Player 1 won this round")
+                        round=round -1
+                        break
+
+                    elif(check_value(str(p1.deck[-1]))<check_value(str(p2.deck[-1]))):
+                        #Add cards onto winners pile
+                        ctr=0
+                        list_won=[]
+                        #Pop cards from loser's deck
+                        while(ctr<5):
+                            i=p1.draw()
+                            ctr=ctr+1
+                            list_won.append(i)
+                            
+                            
+                        p2.add_cards(list_won)
+                        print("Player 2 won this round")
+                        round=round-1
+                        break
+
+                    elif(check_value(str(p1.deck[-1]))==check_value(str(p2.deck[-1]))):
+                        round=+1
+                        break
+  
+    
 main()
 
