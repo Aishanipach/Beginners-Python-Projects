ea86b8ad5848f2ece1a04742ca6448b98c070beb
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2021-01-02 18:47:45 +0530

    Create Get_img.py

---
+++ Web scraping/Get_img.py
@@ -0,0 +1,22 @@
+import requests
+import bs4
+
+resultt= requests.get("https://en.wikipedia.org/wiki/Grace_Hopper")
+sou=bs4.BeautifulSoup(resultt.text, "lxml")
+img_src=""
+y=0
+for x in (sou.select('.thumbimage')): 
+    y=y+1
+    if y==2:  #selects the third listed image on the page
+        img_src=x['src']
+        print(img_src)
+        break
+        
+image_l=requests.get('https:'+img_src)
+#print(image_l.content)  #to display contents of image's binary file
+
+f=open("Mynewimage.jpg", 'wb')
+f.write(image_l.content)
+f.close()
+
+#This program saves the image in the folder where your python file exists
