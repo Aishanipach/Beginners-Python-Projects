d93756a54284d62387df163b2cb31274ed0f5623
Kai Shays <kaishays@Melissas-MacBook-Air.local>
2023-08-24 09:45:46 -0700

    Adds total prime number counter

--- Count_primes/Count_primes.py
+++ Count_primes/Count_primes.py
@@ -1,5 +1,6 @@
 def count_primes2(num):
     primes = [2]
+
     x = 3
     if num < 2:
         return 0
@@ -12,7 +13,14 @@ def count_primes2(num):
             primes.append(x)
             x += 2
     print(primes)
+     #added amount_primes variable 
+    amount_primes = len(primes)
+    #lists how many prime numbers in range
+    print("The amount of prime numbers between 0 and " + str(num) + " is " + str(amount_primes))
+
+
     return len(primes)
 
 num= int(input("Enter limit: "))
 count_primes2(num)
+
