a359174a2ac359517d6cc37ab6ef2ec20a1a0b29
jazzpher <80498982+jazzpher@users.noreply.github.com>
2023-10-22 13:44:47 +0800

    Update Finalprogram.py
    - adds a "Positions" label below the board to help players easily identify the position numbers for their moves.

--- TicTacToe/Finalprogram.py
+++ TicTacToe/Finalprogram.py
@@ -1,25 +1,20 @@
-#from IPython.display import clear_output
+import random
 
 def display_board(board):
-#    clear_output()   (This only works in jupyter!)
-    print("\n"*100)
-    
+    print("\n" * 100)
     print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
     print('-----------')
-    print('   |   |')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('   |   |')
     print('-----------')
-    print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
-
-test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
-
-#------------------------------------------------------------------------------------------
+    print("\nPositions:")
+    print("7 | 8 | 9")
+    print("4 | 5 | 6")
+    print("1 | 2 | 3")
 
 def player_input():
     marker = ''
@@ -32,34 +27,18 @@ def player_input():
     else:
         return ('O', 'X')
 
-player_input()
-
-#---------------------------------------------------------------------------------------------
-
 def place_marker(board, marker, position):
     board[position] = marker
 
-place_marker(test_board,'$',8)
-display_board(test_board)
-
-#---------------------------------------------------------------------------------------------
-
-def win_check(board,mark):
-    
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
-    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
-    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
-    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
-    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
-    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
-    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
-    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
-
-win_check(test_board,'X')
-
-#---------------------------------------------------------------------------------------------
-
-import random
+def win_check(board, mark):
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or
+            (board[4] == mark and board[5] == mark and board[6] == mark) or
+            (board[1] == mark and board[2] == mark and board[3] == mark) or
+            (board[7] == mark and board[4] == mark and board[1] == mark) or
+            (board[8] == mark and board[5] == mark and board[2] == mark) or
+            (board[9] == mark and board[6] == mark and board[3] == mark) or
+            (board[7] == mark and board[5] == mark and board[3] == mark) or
+            (board[9] == mark and board[5] == mark and board[1] == mark))
 
 def choose_first():
     if random.randint(0, 1) == 0:
@@ -67,40 +46,29 @@ def choose_first():
     else:
         return 'Player 1'
 
-    #---------------------------------------------------------------------------------------------
-    
 def space_check(board, position):
-    
     return board[position] == ' '
 
-#---------------------------------------------------------------------------------------------
-
 def full_board_check(board):
-    for i in range(1,10):
+    for i in range(1, 10):
         if space_check(board, i):
             return False
     return True
 
-#---------------------------------------------------------------------------------------------
-
 def player_choice(board):
     position = 0
     
-    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+    while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         position = int(input('Choose your next position: (1-9) '))
         
     return position
 
-#---------------------------------------------------------------------------------------------
-
 def replay():
-    
     return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
 
 print('Welcome to Tic Tac Toe!')
 
 while True:
-    # Reset the board
     theBoard = [' '] * 10
     player1_marker, player2_marker = player_input()
     turn = choose_first()
@@ -115,8 +83,6 @@ while True:
 
     while game_on:
         if turn == 'Player 1':
-            # Player1's turn.
-            
             display_board(theBoard)
             position = player_choice(theBoard)
             place_marker(theBoard, player1_marker, position)
@@ -132,10 +98,7 @@ while True:
                     break
                 else:
                     turn = 'Player 2'
-
         else:
-            # Player2's turn.
-            
             display_board(theBoard)
             position = player_choice(theBoard)
             place_marker(theBoard, player2_marker, position)
@@ -154,4 +117,3 @@ while True:
 
     if not replay():
         break
-
